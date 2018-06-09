d17549074bc6cf9f6408230f077e1fc506d267f0
Ribhararnus Pracutian <oknoorap@gmail.com>
2018-06-09 09:50:30 +0700

    add: maintenance mode

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import styled from 'styled-components'
 import Table from 'react-handsontable'
 import Hotkeys from 'react-hot-keys'
 import Clipboard from 'clipboard'
-import firebase from 'firebase/app'
-import 'firebase/firestore'
 import randomize from 'lodash.random'
-import firebaseConfig from './firebase'
 
-const Wrapper = styled.div`
+const Container = styled.div`
   width: 70%;
   margin: 0 auto;
   display: flex;
@@ -49,8 +46,8 @@ const Tip = styled.div`
   }
 `
 
-const TablePlaceholder = styled.div`
-  min-height: 500px;
+const Placeholder = styled.div`
+  min-height: 350px;
   display: flex;
   justify-content: center;
   align-content: center;
@@ -65,46 +62,53 @@ const CopyElement = styled.button`
   transform: translateY(-100%);
 `
 
+const Wrapper = props => (
+  <Container>
+    <Title>Cozy Space</Title>
+    <Description>Random &quot;Home Design&quot;&mdash;Related Topics Keyword Generator. <a href="https://github.com/oknoorap/cozyspace" rel="noopener noreferrer" target="_blank">Repo</a></Description>
+    <Tip>
+      Press <code>Space</code> to generate new keyword.<br />
+      Press<code>ctrl/cmd+shift+c</code> to copy all keyword.<br />
+    </Tip>
+    {props.children}
+  </Container>
+)
+
 class App extends Component {
   state = {
     data: [],
     longtail: false,
-    loading: false
+    loading: false,
+    error: false,
+    errorMsg: ''
   }
 
   constructor(props) {
     super(props)
-    
-    firebase.initializeApp(firebaseConfig)
-    this.db = firebase.firestore()
-    this.db.settings({
-      timestampsInSnapshots: true
-    })
-
     this.copyEl = React.createRef()
   }
 
   componentDidMount() {
-    new Clipboard(this.copyEl)
-    setTimeout(() => {
+    if (!this.props.maintenance) {
+      new Clipboard(this.copyEl)
       this.fetch()
-    }, 200)
+    }
   }
 
   fetch = () => {
     const minNumber = randomize(0, 100000)
-    const maxNumber = randomize(minNumber, 100000)
-    let state = Object.assign({}, this.state)
+    const maxNumber = randomize(0, 100000)
+    const state = Object.assign({}, this.state)
 
     state.loading = true
     this.setState({
       ...state
     })
 
-    this.db
+    this.props.db
       .collection('keywords')
-      .where('seed', '>=', minNumber)
-      .where('seed', '<=', maxNumber)
+      .where('seed', '>=', Math.min(minNumber, maxNumber))
+      .where('seed', '<=', Math.max(minNumber, maxNumber))
       .limit(100)
       .get()
       .then(snapshot => {
@@ -126,7 +130,12 @@ class App extends Component {
           ...state
         })
       }).catch(err => {
-        throw err
+        state.error = true
+        state.errorMsg = err.message
+
+        this.setState({
+          ...state
+        })
       })
   }
 
@@ -141,6 +150,16 @@ class App extends Component {
   }
 
   render() {
+    if (this.props.maintenance) {
+      return (
+        <Wrapper>
+          <Placeholder>
+            Doing maintenance
+          </Placeholder>
+        </Wrapper>
+      )
+    }
+
     return (
       <Fragment>
         <Hotkeys
@@ -156,16 +175,15 @@ class App extends Component {
           innerRef={component => this.copyEl = component} />
 
         <Wrapper>
-          <Title>Cozy Space</Title>
-          <Description>Random &quot;Home Design&quot;&mdash;Related Topics Keyword Generator. <a href="https://github.com/oknoorap/cozyspace" rel="noopener noreferrer" target="_blank">Repo</a></Description>
-          <Tip>
-            Press <code>Space</code> to generate new keyword.<br />
-            Press<code>ctrl/cmd+shift+c</code> to copy all keyword.<br />
-          </Tip>
-
           {
             this.state.data.length === 0 || this.state.loading
-              ? <TablePlaceholder>Fetching Data..., waiting so long? press space again.</TablePlaceholder>
+              ? <Placeholder>
+                {
+                  this.state.error
+                    ? this.state.errorMsg
+                    : 'Fetching Data..., waiting so long? press space again.'
+                }
+              </Placeholder>
               : <Table data={this.state.data} readOnly={true} height="340" stretchH="all" />
           }
         </Wrapper>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// import firebase from 'firebase/app'
+// import 'firebase/firestore'
+// import firebaseConfig from './firebase'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import './global.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App maintenance={true} />, document.getElementById('root'))
 registerServiceWorker()
